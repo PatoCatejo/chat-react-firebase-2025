c9f6784848425b4c3cd5d69b31e1f8be479a4ecd
PatoCatejo <p.catejoj@gmail.com>
2026-01-06 17:09:46 -0300

    reglas seguridad

--- src/hooks/use-auth-actions.ts
+++ src/hooks/use-auth-actions.ts
@@ -110,6 +110,9 @@ export const useAuthActions = () => {
     setLoading(true);
     try {
       await signOut(auth);
+
+      window.location.href = "/auth/login";
+
       return {
         success: true,
         error: null,
